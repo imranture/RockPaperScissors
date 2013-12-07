a7f4071ac5c5bdddfdb8a19f520b86d776e2e4cc
Imran Ture <imranture@gmail.com>
2013-12-07 15:39:13 +0200

    Rock Paper Scissors Game

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,4 +1,4 @@
-var userInput = function() {
+var userInputsss = function() {
     var userChoice = prompt("Do you choose rock, paper or scissors?");
 if (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
     compare(userChoice,computerChoice);
